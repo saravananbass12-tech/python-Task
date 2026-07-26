e03d142830f4f1f7a27bc5dcebf85823e18a56bc
𝚂𝙰𝚁𝙰𝚅𝙰𝙽𝙰𝙽.𝙳 <saravananbass12@gmail.com>
2026-07-26 11:53:37 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,126 @@
+#task 19
+mark=450
+total=500
+percentage= (mark/total)*100
+print("percentage ;",percentage)
+
+#task 20
+
+distance=240
+time=4
+speed=distance/time
+print("speed ;",speed)
+
+#operator juice
+
+juice=40
+sells=8
+amount=8*40
+print("amount ;",amount)
+
+#PIZZA
+
+Pizza=250
+calculate=4
+total_pizza=4*250
+print("total_pizza ;",total_pizza)
+
+#pen shop
+
+sells=5
+costing=10
+total=5*10
+print("total =",total)
+
+#student
+
+notebook=40
+book=55
+book=65
+calculate_book=44+55+65
+print("calculate_book ;",calculate)
+
+#task 1
+
+wallet=500
+wallet-=120
+print(wallet)
+
+#task 2
+
+salary=0
+salary+=3000
+print(salary)
+
+#task 3
+
+water=150
+water+=50
+print(water)
+
+#task 4
+battery=90
+using=25
+battery-=using
+print("battery;",battery)
+
+
+#task 5
+
+student=7
+intial=2000
+student*=intial
+print("student ;",student)
+
+#task 6
+
+price=150
+initial=850
+price+=initial
+print("price ;",price)
+
+#task 7
+travels=120
+distance=30
+travels+=distance
+print("travels;",travels)
+
+#task 8
+
+points=95
+score=15
+points-=score
+print("points ;",points)
+
+#task 9
+
+number=12
+using=5
+number*=using
+print("number ;",number)
+
+#task 10
+
+chocolate=40
+remaining=10
+chocolate-=remaining
+print("chocolate ;",chocolate)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
